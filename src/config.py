f7808e9f6b2ea5aa3f57f5469cb306b5fff598fc
Paulo Vitor Cruz <paulocsvitor@gmail.com>
2021-02-09 16:04:53 -0300

    improve testing config

--- src/config.py
+++ src/config.py
@@ -1,10 +1,11 @@
 from os import environ
 
 class DefaultConfig:
+    DEBUG = False
     APP_NAME = environ.get('APP_NAME', default='Basic Python Starter')
     PUSHOVER_TOKEN = environ.get('PUSHOVER_TOKEN')
     PUSHOVER_USER = environ.get('PUSHOVER_USER')
-    DEBUG = False
+    IS_TESTING=False
 
 class Development(DefaultConfig):
     DEBUG = True
@@ -13,7 +14,9 @@ class Production(DefaultConfig):
     ...
 
 class Testing(DefaultConfig):
-    TESTING=True
+    IS_TESTING=True
+    PUSHOVER_TOKEN = environ.get('PUSHOVER_TOKEN_TEST', default=DefaultConfig.PUSHOVER_TOKEN)
+    PUSHOVER_USER = environ.get('PUSHOVER_USER_TEST', default=DefaultConfig.PUSHOVER_USER)
 
 def get_env(env = None):
     envs = {
@@ -23,7 +26,7 @@ def get_env(env = None):
         'testing': Testing
     }
 
-    selected_env = env if env != None else environ.get('Env', default='production')
+    selected_env = env if env != None else environ.get('ENV', default='production')
     return envs[selected_env]
 
 env = get_env()
